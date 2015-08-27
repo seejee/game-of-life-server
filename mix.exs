09f9c4b2ae5811f4c4eb3fba1c34109acf2c4e50
Chris Geihsler <chris@geihsler.net>
2015-08-27 06:14:04 -0400

    Phoenix 0.12.0

--- mix.exs
+++ mix.exs
@@ -29,11 +29,11 @@ defmodule ConwayServer.Mixfile do
   # Type `mix help deps` for examples and options
   defp deps do
     [
-     {:phoenix, "0.11.0"},
-     {:plug,    "0.11.3"},
+     {:phoenix, "0.12.0"},
+     {:plug,    "0.12.1"},
      {:exprof,  github: "parroty/exprof"},
      {:cowboy,  "~> 1.0"},
-     {:phoenix_live_reload, "~> 0.3"},
+     {:phoenix_live_reload, "~> 0.3.3"},
    ]
   end
 
